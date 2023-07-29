d826f40875d8649616852f5ec37235d7909b238d
Afif Shomali <shomali2@illinois.edu>
2023-07-28 23:01:27 -0500

    Updated data

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ async function chart1() {
 
     // setting up canvas for chart
     const margins = { top: 10, right: 10, bottom: 50, left: 100 };
-    const width = 1000 - margins.left - margins.right;
+    const width = 1500 - margins.left - margins.right;
     const height = 1000 - margins.top - margins.bottom;
 
     const svg = d3.select("#chart1").append("svg")
@@ -12,8 +12,6 @@ async function chart1() {
         .attr("height", height + margins.top + margins.bottom)
         .append("g")
         .attr("transform", "translate("+ margins.left +", "+ margins.top +")");
-
-
     
     // Adding axes
     const xScale = d3.scaleLog()
@@ -25,16 +23,21 @@ async function chart1() {
         .range([height, 0]);
 
     svg.append("g")
-        .attr("transform", "translate(0, 872)")
+        .attr("transform", "translate(0, 882)")
         .call(d3.axisBottom(xScale));
 
     svg.append("g")
         .call(d3.axisLeft(yScale));
 
+    // Scale for radius values
+    const radiusScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => +d.Population)) 
+        .range([10, 25]); 
+
     // Adding axes titles
     svg.append("text")
         .attr("x", width / 2)
-        .attr("y", height)
+        .attr("y", height + 50)
         .style("text-anchor", "middle")
         .text("Total Jobs Lost");
     
@@ -47,7 +50,7 @@ async function chart1() {
         .text("Cases");
 
     // Add invisble tooltip
-    const tooltip = d3.select("#main")
+    const tooltip = d3.select("#chart1")
         .append("div")
         .attr("class", "tooltip")
         .style("opacity", 0)
@@ -58,6 +61,7 @@ async function chart1() {
         .style("height", "80px")
 
     // Add Data & tooltip functionality
+    // TODO: Add annotations For two below x axis & california & add regions so that we can color based on region 
     svg.selectAll("circle")
         .data(data)
         .enter()
@@ -66,11 +70,16 @@ async function chart1() {
             return xScale(+d.cases);
         })
         .attr("cy", function(d) {
-            return yScale(+d["Total Job Loss Index"])
+            return yScale(+d["Total Job Loss Index"]);
         })
-        .attr("r", 5)
+        .attr("r", function(d) {
+            return radiusScale(+d.Population);
+        })
         .attr("fill", "steelblue")
         .on("mouseover", (event, d) => {
+            d3.select(event.target).attr("stroke", "black")
+            .attr("stroke-width", 2);
+
             tooltip.transition().duration(200).style("opacity", 0.9);
             tooltip.html(
               `<p>State: ${d.state}<br>
@@ -83,7 +92,8 @@ async function chart1() {
               .style("left", (event.pageX+10) + "px")
               .style("top", (event.pageY) + "px");
           })
-          .on("mouseout", () => {
+          .on("mouseout", (event) => {
+            d3.select(event.target).attr("stroke", "none");
             tooltip.transition().duration(500).style("opacity", 0);
           });
     
