c4a2b83c6b3cd3884c63669966130ebfc8c3fbea
Afif Shomali <shomali2@illinois.edu>
2023-07-29 20:40:49 -0500

    Finished second chart

--- scripts.js
+++ scripts.js
@@ -224,11 +224,198 @@ async function chart2() {
     const width = 1500 - margins.left - margins.right;
     const height = 1000 - margins.top - margins.bottom;
 
-    const svg = d3.select("#chart1").append("svg")
+    const svg = d3.select("#chart2").append("svg")
         .attr("width", width + margins.left + margins.right)
         .attr("height", height + margins.top + margins.bottom)
         .append("g")
         .attr("transform", "translate("+ margins.left +", "+ margins.top +")");
+    
+    // Adding axes
+    const xScale = d3.scaleLinear()
+        .domain([10, 220])
+        .range([0, width]);
+    
+    const yScale = d3.scaleLinear()
+        .domain([-4000, 13000])
+        .range([height, 0]);
+
+    svg.append("g")
+        .attr("transform", "translate(0, 720)")
+        .call(d3.axisBottom(xScale));
+
+    svg.append("g")
+        .call(d3.axisLeft(yScale));
+
+    // Scale for radius values
+    const radiusScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => +d.Population)) 
+        .range([10, 25]); 
+
+    // Adding axes titles
+    svg.append("text")
+        .attr("x", width / 2)
+        .attr("y", height + 40)
+        .style("text-anchor", "middle")
+        .text("Deaths Per 100k");
+    
+
+    svg.append("text")
+        .attr("x", -height / 2)
+        .attr("y", -70)
+        .attr("transform", "rotate(-90)")
+        .style("text-anchor", "middle")
+        .text("Jobs Lost Per 100k Employed");
+
+    // Add invisble tooltip
+    const tooltip = d3.select("#chart2")
+        .append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0)
+        .style("background-color", "grey")
+        .style("padding", "5")
+        .style("color", "white")
+        .style("width", "150px")
+        .style("height", "80px")
+
+    // Add colors for regions
+    const regioncolors = d3.scaleOrdinal()
+        .domain([...new Set(data.map(d => d.Region))])
+        .range(d3.schemeCategory10);
+
+    // Add Data & tooltip functionality
+    // TODO: Add annotations For two below x axis & california & add regions so that we can color based on region 
+    svg.selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", function(d) {
+            return xScale((+d.deaths / +d.Population) * 100000 );
+        })
+        .attr("cy", function(d) {
+            return yScale((+d["Total Job Loss Index"]/ +d.total_li_workers_employed) * 100000);
+        })
+        .attr("r", function(d) {
+            return radiusScale(+d.Population);
+        })
+        .attr("fill", function(d) {
+            return regioncolors(d.Region)
+        })
+        .on("mouseover", (event, d) => {
+            d3.select(event.target).attr("stroke", "black")
+            .attr("stroke-width", 2);
+
+            tooltip.transition().duration(200).style("opacity", 0.9);
+            tooltip.html(
+              `<p>State: ${d.state}<br>
+               <p>Deaths Per 100k: ${Math.round((+d.deaths / +d.Population) * 100000)}<br>
+               <p>Jobs Lost Per 100k Employed: ${Math.round((+d["Total Job Loss Index"]/ +d.total_li_workers_employed) * 100000)}<br>
+               <p>Total Job Losses: ${d["Total Job Loss Index"]}<br>
+               <p>Population: ${d.Population}</p>`
+            )
+              .style("left", (event.pageX+10) + "px")
+              .style("top", (event.pageY) + "px");
+          })
+          .on("mouseout", (event) => {
+            d3.select(event.target).attr("stroke", "none");
+            tooltip.transition().duration(500).style("opacity", 0);
+          });
+          
+    // Add State Codes to each circle
+    svg.selectAll(".statecode")
+        .data(data)
+        .enter()
+        .append("text")
+        .attr("font-size", 10)
+        .attr("fill", "white")
+        .attr("x", function(d) {
+            return xScale((+d.deaths / +d.Population) * 100000);
+        })
+        .attr("y", function(d) {
+            return yScale((+d["Total Job Loss Index"]/ +d.total_li_workers_employed) * 100000);
+        })
+        .style("text-anchor", "middle")
+        .attr("alignment-baseline", "middle")
+        .text(function(d) {
+            return d["State Code"];
+        });
+
+    //Create legend
+    const legend = svg.append("g")
+        .attr("class", "legend")
+        .attr("transform", "translate(20, 20)");
+      
+    
+    const regions = [...new Set(data.map(d => d.Region))];
+    const legendItems = legend.selectAll(".legend-item")
+        .data(regions)
+        .enter()
+        .append("g")
+        .attr("class", "legend-item")
+        .attr("transform", (d, i) => "translate(0," + (i * 20) + ")");
+      
+    legendItems.append("circle")
+        .attr("cx", 10)
+        .attr("cy", 10)
+        .attr("r", 4)
+        .attr("fill", regioncolors);
+      
+    legendItems.append("text")
+        .attr("x", 25)
+        .attr("y", 10)
+        .style("fill", regioncolors)
+        .style("font-size", "12px") 
+        .attr("alignment-baseline", "middle")
+        .text(d => d);
+    
+    // Add a title for the legend
+    legend.append("text")
+        .attr("x", 7)
+        .attr("y", -8)
+        .style("font-size", "14px")
+        .style("font-weight", "bold")
+        .text("Region");
+    
+        const annotations = [{
+            x: 1140,
+            y: 200,
+            note: {
+                label: "States in the North East region had high job loss rates",
+                bgPadding: {"top":15,"left":10,"right":10,"bottom":10},
+                title: "North East",
+                orientation: "middle",
+                align: "left"
+            },
+            type: d3.annotationCalloutCircle,
+            dx: -350,
+            dy: -50,
+            subject: {
+                radius: 200,
+            }
+        },
+        {
+            x: 750,
+            y: 299,
+            note: {
+                label: "",
+                bgPadding: {"top":15,"left":10,"right":10,"bottom":10},
+                orientation: "middle",
+                align: "left"
+            },
+            type: d3.annotationCalloutCircle,
+            dx: 40,
+            dy: -150,
+            subject: {
+                radius: 20,
+            }
+        },
+        ]
+    
+        const makeAnnotations = d3.annotation()
+            .annotations(annotations);
+        
+        svg.append("g")
+            .attr("class", "annotation-group")
+            .call(makeAnnotations);
 
     
 }
